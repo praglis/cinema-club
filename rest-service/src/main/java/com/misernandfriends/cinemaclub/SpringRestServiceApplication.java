15a5469250c65b28648445a582fa6e144a0a5a9a
kacper <namishis2@gmail.com>
2019-10-21 20:46:52 +0200

    Init commit

---
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/SpringRestServiceApplication.java
@@ -0,0 +1,14 @@
+package com.misernandfriends.cinemaclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestServiceApplication.class, args);
+    }
+}
---
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/entites/Greeting.java
@@ -0,0 +1,13 @@
+package com.misernandfriends.cinemaclub.entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+    public int id;
+    public String name;
+}
