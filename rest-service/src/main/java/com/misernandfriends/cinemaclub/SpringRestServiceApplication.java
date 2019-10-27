1617a0f9b0047d5f3a4b221cf5e50e9476bd42b5
kacper <namishis2@gmail.com>
2019-10-27 20:47:18 +0100

    Eureka disabled in properties

--- rest-service/src/main/java/com/misernandfriends/cinemaclub/SpringRestServiceApplication.java
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/SpringRestServiceApplication.java
@@ -2,10 +2,10 @@ package com.misernandfriends.cinemaclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class SpringRestServiceApplication {
 
     public static void main(String[] args) {
