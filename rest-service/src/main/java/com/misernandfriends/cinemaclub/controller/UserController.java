a9cb3143e66d75919e9412f49c37bd45d482ce5d
Kacper Barszczewski <namishis2@gmail.com>
2019-12-01 17:02:30 +0100

    Hotfix - impossible to login/register

--- rest-service/src/main/java/com/misernandfriends/cinemaclub/controller/UserController.java
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/controller/UserController.java
@@ -1,19 +1,20 @@
 package com.misernandfriends.cinemaclub.controller;
 
 import com.misernandfriends.cinemaclub.model.user.UserDTO;
-import com.misernandfriends.cinemaclub.repository.user.UserRepository;
 import com.misernandfriends.cinemaclub.serviceInterface.SecurityService;
 import com.misernandfriends.cinemaclub.serviceInterface.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class UserController {
 
     @Autowired
@@ -22,45 +23,35 @@ public class UserController {
     @Autowired
     private SecurityService securityService;
 
-    @GetMapping("/login")
-    public void Username(){
-
-    }
-
-    @GetMapping("/register")
-    public void registration(){
-
-    }
-
     //Przykład do pobierania aktualnego usera
     @GetMapping("/user")
-    public ResponseEntity userName(){
+    public ResponseEntity userName() {
         String currentPrincipalName = securityService.findLoggedInUsername();
-
-        return new ResponseEntity<String>("GET Response : "
-                + currentPrincipalName, HttpStatus.OK);
+        Map<String, String> body = new HashMap<>();
+        body.put("username", currentPrincipalName);
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody UserDTO user) {
         Optional<UserDTO> checkUser = userService.findByUsername(user.getUsername());
-        if(checkUser.isPresent() ){
-            if(checkUser.get().getStatus().equals("C")){
-                return new ResponseEntity(user.getUsername() + " has been closed", HttpStatus.BAD_REQUEST);
+        if (checkUser.isPresent()) {
+            if (checkUser.get().getStatus().equals("C")) {
+                return new ResponseEntity<>(user.getUsername() + " has been closed", HttpStatus.BAD_REQUEST);
             }
-            if(checkUser.get().getStatus().equals("B")){
-                return new ResponseEntity(user.getUsername() + " has been banned", HttpStatus.BAD_REQUEST);
+            if (checkUser.get().getStatus().equals("B")) {
+                return new ResponseEntity<>(user.getUsername() + " has been banned", HttpStatus.BAD_REQUEST);
             }
-        }
-        else{
-            return new ResponseEntity("Username/password is incorrect", HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>("Username/password is incorrect", HttpStatus.BAD_REQUEST);
         }
 
         String transientPassword = user.getPassword();
         securityService.autoLogin(user.getUsername(), transientPassword);
-        return new ResponseEntity("GET Response : "
-                + user.getUsername(), HttpStatus.OK);
+        Map<String, String> body = new HashMap<>();
+        body.put("username", user.getUsername());
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
 
@@ -69,10 +60,10 @@ public class UserController {
         Optional<UserDTO> userExists = userService.findByUsername(user.getUsername());
         Optional<UserDTO> emailExists = userService.findByEmail(user.getEmail());
         if (userExists.isPresent()) {
-            return new ResponseEntity("User already exists", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("User already exists", HttpStatus.BAD_REQUEST);
         }
         if (emailExists.isPresent()) {
-            return new ResponseEntity("Email already exists", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Email already exists", HttpStatus.BAD_REQUEST);
         }
         String transientPassword = user.getPassword();
         Date date = new Date();
@@ -81,11 +72,8 @@ public class UserController {
         user.setType("U");
         userService.save(user);
         securityService.autoLogin(user.getUsername(), transientPassword);
-        return new ResponseEntity<String>("GET Response : "
-                + user.getUsername(), HttpStatus.OK);
+        Map<String, String> body = new HashMap<>();
+        body.put("username", user.getUsername());
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
-
-
-
-
 }
