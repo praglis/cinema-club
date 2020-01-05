29a6ced8bee0d9c409e25aef04edb1aa9349c71c
ernest <erneststachelski@gmail.com>
2020-01-05 11:07:20 +0100

    get popular and best movies from cache

--- rest-service/src/main/java/com/misernandfriends/cinemaclub/SpringRestServiceApplication.java
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/SpringRestServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(CustomProperties.class)
+@EnableCaching
 public class SpringRestServiceApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
