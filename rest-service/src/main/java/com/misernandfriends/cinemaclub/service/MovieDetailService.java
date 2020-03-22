2efcacd025aa6d1529a668ca53a732a37e0e295a
Kacper Barszczewski <namishis2@gmail.com>
2020-03-22 13:52:05 +0100

    CC-70 Remove useless comments

--- rest-service/src/main/java/com/misernandfriends/cinemaclub/service/MovieDetailService.java
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/service/MovieDetailService.java
@@ -10,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 public class MovieDetailService implements MovieFetchServiceLocal {
     @Override
     public String getMovieById(Integer id) {
-//        final String uri = "https://api.themoviedb.org/3/movie/" + id + "?api_key=08529ee2a8448b81018b7519148033ed&language=en-US";
         String uri = new UrlHelper(CacheValue._API_URLS.MOVIES_API_URL).setQuery(id.toString()).build();
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(uri, String.class);
@@ -18,7 +17,6 @@ public class MovieDetailService implements MovieFetchServiceLocal {
 
     @Override
     public String getMovieByQuery(String query) {
-//        final String uri = "https://api.themoviedb.org/3/search/movie?query=" + query + "&api_key=08529ee2a8448b81018b7519148033ed&language=en-US";
         String uri = new UrlHelper(CacheValue._API_URLS.MOVIES_API_URL_QUERY).setQuery(query).build();
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(uri, String.class);
