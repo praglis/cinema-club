c9cb90e4eb63cb3409ed7724b4928b930a2e4b7d
Michał <filonczuk.michal.98@gmail.com>
2020-04-16 00:52:36 +0200

    working, but not everything is tested

---
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/pojo/QuestionnairePostBody.java
@@ -0,0 +1,33 @@
+package com.misernandfriends.cinemaclub.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+public class QuestionnairePostBody {
+
+    @JsonProperty("user_id")
+    @SerializedName("user_id")
+    private Long userId;
+
+    @JsonProperty("page")
+    @SerializedName("page")
+    private Integer page;
+
+    @JsonProperty("results")
+    @SerializedName("results")
+    private List<Movie> movies = new ArrayList<>();
+
+    @JsonProperty("total_results")
+    @SerializedName("total_results")
+    private Integer totalResults;
+
+    @JsonProperty("total_pages")
+    @SerializedName("total_pages")
+    private Integer totalPages;
+}
