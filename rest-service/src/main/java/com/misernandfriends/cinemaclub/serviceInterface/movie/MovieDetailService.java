933b69735c6d508e81cb5acfa9a8897e3703226b
Kacper Barszczewski <namishis2@gmail.com>
2020-05-13 17:54:30 +0200

    CC-127 Person page backend

--- rest-service/src/main/java/com/misernandfriends/cinemaclub/serviceInterface/movie/MovieDetailService.java
+++ rest-service/src/main/java/com/misernandfriends/cinemaclub/serviceInterface/movie/MovieDetailService.java
@@ -1,10 +1,8 @@
 package com.misernandfriends.cinemaclub.serviceInterface.movie;
 
-import com.misernandfriends.cinemaclub.pojo.movie.VideoResults;
-import com.misernandfriends.cinemaclub.pojo.movie.crew.Credits;
 import com.misernandfriends.cinemaclub.pojo.movie.Genres;
-import com.misernandfriends.cinemaclub.pojo.movie.MovieSearchCriteria;
 import com.misernandfriends.cinemaclub.pojo.movie.Movie;
+import com.misernandfriends.cinemaclub.pojo.movie.MovieSearchCriteria;
 import com.misernandfriends.cinemaclub.pojo.movie.MoviesList;
 import com.misernandfriends.cinemaclub.pojo.movie.VideoResults;
 import com.misernandfriends.cinemaclub.pojo.movie.crew.Credits;
@@ -12,20 +10,12 @@ import com.misernandfriends.cinemaclub.pojo.movie.crew.Person;
 
 public interface MovieDetailService {
     Genres getGenres();
-
     Credits getMovieCreditsById(Integer id);
-
     Movie getMovieById(Integer page);
-
     Movie getMovieByLongId(Long id);
-
     MoviesList getMovieByQuery(String query);
-
     MoviesList getMovies(MovieSearchCriteria criteria);
-
     VideoResults getKeyForTrailer(Integer movieId);
-
     Person getPerson(Long id);
-
     Credits getPersonCreditsById(Long id);
 }
